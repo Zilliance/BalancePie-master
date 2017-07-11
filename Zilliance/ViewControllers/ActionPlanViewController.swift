1b8ba55706f04798362779a847e03f77eca18337
Ignacio Zunino <zignacio@gmail.com>
2017-07-11 12:05:38 -0300

    UI design for calendar cell

---
+++ Zilliance/ViewControllers/ActionPlanViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActionPlanViewController.swift
+//  Zilliance
+//
+//  Created by Ignacio Zunino on 11-07-17.
+//  Copyright © 2017 Pillars4Life. All rights reserved.
+//
+
+import UIKit
+
+class ActionPlanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
