2c5e13bc4d8a30790902d64372497ba3831d4d59
Ignacio Zunino <zignacio@gmail.com>
2017-07-14 15:19:51 -0300

    added separations between the cells

--- Zilliance/ViewControllers/ActionPlanViewController.swift
+++ Zilliance/ViewControllers/ActionPlanViewController.swift
@@ -19,7 +19,7 @@ class ActionPlanViewController: UIViewController {
 
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 80
-
+        self.tableView.separatorColor = UIColor.color(forRed: 249, green: 249, blue: 250, alpha: 1)
         // Do any additional setup after loading the view.
         addTestingNotifications()
     }
